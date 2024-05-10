625c5cb3245920d5f5e302a409c9f918825cf159
cwadven <cwadven@naver.com>
2024-05-10 22:42:57 +0900

    Feat: get_tags 사용
    - get_tags 사용

--- board/views.py
+++ board/views.py
@@ -25,7 +25,11 @@ from board.models import (
     Rereply,
     Tag,
 )
-from board.services import get_boards_by_board_group_id, get_active_posts
+from board.services import (
+    get_active_posts,
+    get_boards_by_board_group_id,
+    get_tags,
+)
 from chatgpt.models import Lesson
 from common.common_utils.paginator_utils import web_paging
 from control.models import Announce
@@ -58,19 +62,15 @@ def home(request):
         '-reply_count',
         '-id',
     )[:6]
-
-    tag_set = Tag.objects.all()
-
     announce_set = Announce.objects.order_by(
         '-created_at'
     )[:5]
-
     lesson = Lesson.objects.last()
 
     context = {
         'recent_post_set': recent_post_qs,
         'liked_ordered_post_set': liked_ordered_post_qs,
-        'tag_set': tag_set,
+        'tag_set': get_tags(),
         'announce_set': announce_set,
         'lesson': lesson,
     }
