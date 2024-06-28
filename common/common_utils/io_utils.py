3245878f48b2fbcdcd2f575f434341e02d7cb479
cwadven <cwadven@naver.com>
2024-06-29 02:08:31 +0900

    Feat: Email
    - Email

--- common/common_utils/io_utils.py
+++ common/common_utils/io_utils.py
@@ -1,5 +1,8 @@
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import (
+    EmailMessage,
+    send_mail,
+)
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
@@ -23,3 +26,26 @@ def send_email(title: str, html_body_content: str, payload: dict, to: list) -> N
         html_message=message,
         fail_silently=False,
     )
+
+
+def send_email_with_file(title: str, html_body_content: str, payload: dict, to: str, file_path: str) -> None:
+    """
+    title: 메일 제목
+    html_body_content: 적용할 templates 폴더에 있는 html 파일 위치
+    payload: 해당 template_tag 로 쓰일 값들
+    to: 보낼 사람들 (리스트로 전달 필요)
+    file_path: 첨부할 파일 경로
+    """
+    message = render_to_string(
+        html_body_content,
+        payload
+    )
+    email = EmailMessage(
+        title,
+        strip_tags(message),
+        settings.EMAIL_HOST_USER,
+        [to],
+    )
+    if file_path:
+        email.attach_file(file_path)
+    email.send()
--- common/consts/common_consts.py
+++ common/consts/common_consts.py
@@ -2,6 +2,7 @@ REPLY_NOTIFICATION = 'reply_notification'
 REREPLY_NOTIFICATION = 'rereply_notification'
 LIKE_NOTIFICATION = 'like_notification'
 HEALTH_CHECK = 'health_check'
+BACKUP_SQL = 'backup_sql'
 
 
 EMAIL_TEMPLATE_MAPPER = {
@@ -9,4 +10,5 @@ EMAIL_TEMPLATE_MAPPER = {
     REREPLY_NOTIFICATION: 'email/notification/rereply_notification.html',
     LIKE_NOTIFICATION: 'email/notification/like_notification.html',
     HEALTH_CHECK: 'email/common/health_check.html',
+    BACKUP_SQL: 'email/common/backup_sql.html',
 }
