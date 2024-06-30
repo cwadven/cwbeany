ee7b15b2a617d6b6350a4cf15922e076ca23d6a8
cwadven <cwadven@naver.com>
2024-06-30 18:05:03 +0900

    Feat: title 추가
    - title 추가

--- popup/dtos/object_dtos.py
+++ popup/dtos/object_dtos.py
@@ -10,6 +10,7 @@ from popup.models import Popup
 
 class PopupModalItem(BaseModel):
     id: int = Field(...)
+    title: Optional[str] = Field(...)
     description: Optional[str] = Field(...)
     image_url: Optional[str] = Field(...)
     on_click_link: str = Field(...)
@@ -22,6 +23,7 @@ class PopupModalItem(BaseModel):
     def of(cls, popup: Popup, top=None, left=None):
         return cls(
             id=popup.id,
+            title=popup.title,
             description=popup.description,
             image_url=popup.image.url if popup.image else None,
             on_click_link=popup.on_click_link,
