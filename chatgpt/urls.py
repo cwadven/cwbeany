f9fca92f7396d0ac3c60a08dbd34190524767a2f
이창우(Changwoo Lee) <changwoo.lee@wesang.com>
2024-09-21 18:32:47 +0900

    Feat: Finish summary
    - Finish summary

--- chatgpt/urls.py
+++ chatgpt/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
 
-app_name = 'notification'
+app_name = 'chatgpt'
+
 
 urlpatterns = [
+    path('post-summary/<int:post_id>', get_summary_by_post_id, name='get_summary_by_post_id'),
 ]
--- chatgpt/views.py
+++ chatgpt/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from chatgpt.services import get_latest_post_summary_by_post_id
 
-# Create your views here.
+
+def get_summary_by_post_id(request, post_id: int):
+    if post_id is None:
+        return JsonResponse({'error': 'post_id is required'}, status=400)
+
+    post_summary = get_latest_post_summary_by_post_id(post_id)
+    context = {
+        'status': post_summary.status if post_summary else None,
+        'summary': post_summary.body if post_summary else None,
+    }
+    return JsonResponse(context, status=200)
